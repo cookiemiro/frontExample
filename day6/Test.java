5162f1f263ea7e154aa12da4fd83073557353238
cookiemiro <xodml9598@gmail.com>
2024-08-28 20:46:29 +0900

    day5 & day6

---
+++ day6/Test.java
@@ -0,0 +1,13 @@
+package day6;
+public class Test {
+    public static void main(String[] args) {
+        for (int i = 0; i < 6; i++) {
+            int a = 1;
+            // System.out.println(a);
+
+            if (i > 3) {
+                System.out.println(a);
+            }
+        }
+    }
+}
